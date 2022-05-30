2335a97c445546ba56e96046b9553e767d59d056
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-05-30 11:29:21 -0400

    Update binarySearch.py

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -9,17 +9,16 @@ def recursiveBinarySearch(array, min, max, target, attribute): # Works
     middle = (max+min)//2 # Inaccuracy is compensated for since the search will immediately determine subset.
     
     if array[middle].__dict__[attribute] == target: # If middle is target
-      print('Target is present at index ', middle)
+      return middle
     elif array[middle].__dict__[attribute] > target: # If target is behind
       return recursiveBinarySearch(array, min, middle - 1, target, attribute)
     else: # If target is ahead
       return recursiveBinarySearch(array, middle + 1, max, target, attribute)
   else: # Target is absent
-    print('Target is absent')
+    return -1
 
 # Iterative
 def iterativeBinarySearch(array, min, max, target, attribute): # Works
-  absent = True
   while min <= max:
     middle = (max+min)//2
 
@@ -28,10 +27,6 @@ def iterativeBinarySearch(array, min, max, target, attribute): # Works
     elif array[middle].__dict__[attribute] > target:
       max = middle - 1
     else:
-      print("Target is present at index", middle)
-      absent = False # Using return makes this tedious in main.py
-      break
-  
-  # Since return doesn't work
-  if absent:
-    print("Target is absent")
+      return middle
+
+  return -1 # Will only reach if while loop completes without finding target
