dfd1bd603c554993c1a452c3e6d6663e5b0edb0c
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-05-30 11:28:55 -0400

    Update linearSearch.py

--- Algorithms/linearSearch.py
+++ Algorithms/linearSearch.py
@@ -6,27 +6,24 @@ def iterativeLinearSearch(array, target, attribute): # Works
   for i in range(0, len(array)):
     # If target is found
     if array[i].__dict__[attribute] == target:
-      print("Target is present at index", i) # Return index
-      break
+      return i # Return index
     elif array[i].__dict__[attribute] != array[-1]: # If still being looped
       continue # Keep going
     else: 
-      print("Target is absent")
-
+      return -1
 
 # Recursive
 def recursiveLinearSearch(array, f, l, target, attribute): # Works
   # f and l denote first and last index of the array
   try:
     if l < f:
-      print("Target is absent")
+      return -1
     elif array[f].__dict__[attribute] == target:
-      print("Target is present at index ", f)
+      return f
     elif array[l].__dict__[attribute] == target:
-      print("Target is present at index ", l)
+      return l
     else:
       # Close in from both sides of the list
       return recursiveLinearSearch(array, f+1, l-1, target, attribute)
   except RecursionError:
     print("StackOverflowError\n")
-  
